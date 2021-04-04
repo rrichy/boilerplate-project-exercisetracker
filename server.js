8bc4c5612aa437c608cffc097889fd529d9a7f9f
rrichy <rasulrichy@gmail.com>
2021-04-05 07:40:06 +0800

    final

--- server.js
+++ server.js
@@ -23,16 +23,31 @@ app.post('/api/exercise/new-user', (req, res) => {
   });
 });
 
+// GET List of users
+app.get('/api/exercise/users', (req, res) => {
+  myApp.getList((_, list) => {
+    res.json(list);
+  });
+})
+
 // POST Add exercise
 app.post('/api/exercise/add', (req, res) => {
-  // console.log(Object.assign(req.body, {date: new Date(req.body.date)}));
-  if(req.body.date === '') Object.assign(req.body, {date: Date.now() });
+  if(req.body.date === '') Object.assign(req.body, {date: new Date()});
+
   myApp.addExercise(req.body, (err, user) => {
     if(err) res.send(`Cast to ${err.kind} failed for value "${err.value}" at path "${err.path}" for model "et_user"`);
     else if(user === null) res.send('Unknown userId');
     else res.json(user);
   });
 })
+
+// GET exercise log from Id
+app.get('/api/exercise/log', (req, res) => {
+  myApp.getLog(req.query, (err, log) => {
+    if(err) console.log(err);
+    res.json(log);
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
